4032111981a684cad9abd77caf7d3657268464ff
Daniel Washington <danielvertigo@hotmail.com>
2021-06-04 15:43:29 -0300

    Files

--- DoubleLetters/ViewController.swift
+++ DoubleLetters/ViewController.swift
@@ -5,6 +5,20 @@
 //  Created by Daniel Washington Ignacio on 04/06/21.
 //
 
+/*
+ Create a function that takes a word and returns true if the word has two consecutive identical letters.
+
+ Examples
+
+ doubleLetters("loop") ➞ true
+
+ doubleLetters("yummy") ➞ true
+
+ doubleLetters("orange") ➞ false
+
+ doubleLetters("munchkin") ➞ false
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +26,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //print(self.doubleLetters("loop"))
+        //print(self.doubleLetters("yummy"))
+        //print(self.doubleLetters("orange"))
+        print(self.doubleLetters("munchkin"))
     }
 
-
+    func doubleLetters(_ word: String) -> Bool {
+        var arr: [Character] = []
+        for n in word{
+            arr.append(n)
+        }
+        var size: Int = arr.count
+        for n in 0..<size-1{
+            if arr[n] == arr[n+1]{
+                return true
+            }
+        }
+       return false
+    }
+    
+    
 }
 
